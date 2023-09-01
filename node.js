9318ddb8a6918ff2b33b0922eae6077ac9d5de3b
Rodrigo Reis <rodrigosor@gmail.com>
2023-09-01 00:14:53 -0300

    chore: add airbnb-base

--- node.js
+++ node.js
@@ -1,31 +1,38 @@
 module.exports = {
-    env: {
-      es2021: true,
-      node: true,
-    },
-    extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          printWidth: 80,
-          tabWidth: 2,
-          singleQuote: true,
-          trailingComma: 'all',
-          arrowParens: 'always',
-          semi: false,
-        },
-      ],
-    },
-    settings: {
-      'import/parsers': {
-        [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+  env: {
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:n/recommended',
+    'plugin:promise/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'n', 'promise'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: false,
       },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
-  }
+  },
+}
